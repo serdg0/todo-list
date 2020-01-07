ed4b8e811449dd30b0b404f21184e0f417470fb8
serdg0 <sergio.diazg92@gmail.com>
2020-01-07 17:19:42 -0600

    fixed default dates

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import project from './project';
 import task from './task';
 import './css/style.css';
 import { addProject, displayInt } from './dom';
+import { format } from 'date-fns'
+import { getPriority } from 'os';
 
 const strengthRoutine = project({
     title: 'Workout',
@@ -13,35 +15,35 @@ addProject(strengthRoutine);
 strengthRoutine.addTask(task({
     title: 'Always remember to breath',
     description: 'In for excercise mechanic, and out for return to initial position',
-    dueDate: Date.now(),
+    dueDate: format(Date.now(), 'yyyy/MM/dd'),
     priority: 'high',
 }))
 
 strengthRoutine.addTask(task({
     title: '10 pushups',
     description: 'arms at shoulder level, legs spread for stability',
-    dueDate: Date.now(),
+    dueDate: format(Date.now(), 'yyyy/MM/dd'),
     priority: 'high',
 }))
 
 strengthRoutine.addTask(task({
     title: '20 jumping jacks',
     description: 'try to do them as fast as possible',
-    dueDate: Date.now(),
+    dueDate: format(Date.now(), 'yyyy/MM/dd'),
     priority: 'high',
 }))
 
 strengthRoutine.addTask(task({
     title: '60 secs of rope skip',
     description: 'remember that legs are not stretched',
-    dueDate: Date.now(),
+    dueDate: format(Date.now(), 'yyyy/MM/dd'),
     priority: 'high',
 }))
 
 strengthRoutine.addTask(task({
     title: 'Rest and repeat',
     description: '3 times for beginners, 5 for advanced',
-    dueDate: Date.now(),
+    dueDate: format(Date.now(), 'yyyy/MM/dd'),
     priority: 'high',
 }))
 
